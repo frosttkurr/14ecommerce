ca1f29f9f3eefcf6b7f91175667805c7a3dac020
frosttkurr <thisiscyber13@gmail.com>
2021-05-30 22:21:02 +0800

    admin-navbar

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -57,9 +57,6 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span>pastibisa08</span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
 							<ul class="dropdown-menu">
-								<li><a href="#"><i class="lnr lnr-user"></i> <span>My Profile</span></a></li>
-								<li><a href="#"><i class="lnr lnr-envelope"></i> <span>Message</span></a></li>
-								<li><a href="#"><i class="lnr lnr-cog"></i> <span>Settings</span></a></li>
 								<li><a href="{{ route('admin.logout') }}"><i class="lnr lnr-exit"></i> <span>Logout</span>
                                 </a></li>
 							</ul>
